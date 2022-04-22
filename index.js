622184446d26ca77a08a440b027cd8f66bc525ed
David Konsumer <konsumer@jetboystudio.com>
2022-04-22 01:26:33 -0700

    trying to fix gamepad on gpi

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ r.SetTraceLogLevel(r.LOG_ERROR)
 r.InitWindow(320, 240, 'Pakémon')
 r.SetTargetFPS(60)
 
+r.SetGamepadMappings(`
+78696e70757401000000000000000000,XInput Gamepad (GLFW),platform:Windows,a:b0,
+b:b1,x:b2,y:b3,leftshoulder:b4,rightshoulder:b5,back:b6,start:b7,leftstick:b8,
+rightstick:b9,leftx:a0,lefty:a1,rightx:a2,righty:a3,lefttrigger:a4,
+righttrigger:a5,dpup:h0.1,dpright:h0.2,dpdown:h0.4,dpleft:h0.8,
+`)
+
 // https://docs.libretro.com/guides/input-and-controls/
 const mappedInput = {
   A: { gamepad: 0, key: r.KEY_X },
