99996b741dd0c09c479841f1317bd13385c6d23c
David Konsumer <konsumer@jetboystudio.com>
2022-04-22 20:36:33 -0700

    better gamepad

--- index.js
+++ index.js
@@ -4,44 +4,36 @@ import * as SceneGamesTop from './scenes/games_top.js'
 
 r.SetTraceLogLevel(r.LOG_ERROR)
 r.InitWindow(320, 240, 'Pakémon')
-r.SetTargetFPS(60)
+
+// later, these will be in upstream node-raylib
+export const wait = time => new Promise((resolve, reject) => { setTimeout(resolve, time) })
+export async function MaintainFPS (targetFPS) {
+  const timetarget = 1 / targetFPS
+  const timeframe = r.GetFrameTime()
+  const waittime = (timetarget - timeframe) * 2000
+  const realFPS = (1 / timeframe) * 2
+  if (waittime > 0) {
+    await wait(waittime)
+  }
+  return r.WindowShouldClose() ? false : realFPS
+}
+
 
 // https://docs.libretro.com/guides/input-and-controls/
 const mappedInput = {
-  A: { gamepad: 0, key: r.KEY_X },
-  B: { gamepad: 1, key: r.KEY_Z },
-  X: { gamepad: 2, key: r.KEY_S },
-  Y: { gamepad: 3, key: r.KEY_A },
-  L: { gamepad: 4, key: r.KEY_Q },
-  R: { gamepad: 5, key: r.KEY_W },
-  SELECT: { gamepad: 6, key: r.KEY_LEFT_SHIFT },
-  START: { gamepad: 7, key: r.KEY_ENTER },
-  UP: { key: r.KEY_UP },
-  DOWN: { key: r.KEY_DOWN },
-  LEFT: { key: r.KEY_LEFT },
-  RIGHT: { key: r.KEY_RIGHT }
+  A: { button: 0, key: r.KEY_X },
+  B: { button: 1, key: r.KEY_Z },
+  X: { button: 2, key: r.KEY_S },
+  Y: { button: 3, key: r.KEY_A },
+  L: { button: 4, key: r.KEY_Q },
+  R: { button: 5, key: r.KEY_W },
+  SELECT: { button: 6, key: r.KEY_LEFT_SHIFT },
+  START: { button: 7, key: r.KEY_ENTER },
+  UP: { axis: 0, direction: -1, key: r.KEY_UP },
+  DOWN: { axis: 0, direction: 1, key: r.KEY_DOWN },
+  LEFT: { axis: 1, direction: -1, key: r.KEY_LEFT },
+  RIGHT: { axis: 1, direction: 1, key: r.KEY_RIGHT }
 }
-
-/*
-use these, at some point, make sure buttons are setup right:
-
-14 - GAMEPAD_BUTTON_MIDDLE
-15 - GAMEPAD_BUTTON_MIDDLE_RIGHT
-13 - GAMEPAD_BUTTON_MIDDLE_LEFT
-
-8 - GAMEPAD_BUTTON_RIGHT_FACE_LEFT
-7 - GAMEPAD_BUTTON_RIGHT_FACE_DOWN
-6 - GAMEPAD_BUTTON_RIGHT_FACE_RIGHT
-5 - GAMEPAD_BUTTON_RIGHT_FACE_UP
-
-1 - GAMEPAD_BUTTON_LEFT_FACE_UP
-2 - GAMEPAD_BUTTON_LEFT_FACE_RIGHT
-3 - GAMEPAD_BUTTON_LEFT_FACE_DOWN
-4 - GAMEPAD_BUTTON_LEFT_FACE_LEFT
-
-9 - GAMEPAD_BUTTON_LEFT_TRIGGER_1
-11 - GAMEPAD_BUTTON_RIGHT_TRIGGER_1
-*/
 
 let currentScene
 globalThis.shouldClose = false
@@ -58,119 +50,73 @@ global.switchScene = (scene, params = {}) => {
 
 global.switchScene(SceneGamesTop)
 
-function exit () {
-  globalThis.shouldClose = true
-  clearInterval(interval)
-  if (currentScene?.destroy) {
-    currentScene.destroy()
+const pushOnce = {}
+
+while(await MaintainFPS(60)) {
+  // allow global exit with START + SELECT
+  if (
+    (r.IsKeyDown(mappedInput.START.key) && r.IsKeyDown(mappedInput.SELECT.key)) ||
+    (r.IsGamepadButtonDown(0, mappedInput.START.button) && r.IsGamepadButtonDown(0, mappedInput.SELECT.button))
+  ) {
+    break
   }
-  r.CloseWindow()
+
+  // map input to callbacks
+  for (const button of Object.keys(mappedInput)) {
+    if (currentScene.buttonPress) {
+      if (typeof mappedInput[button].button !== 'undefined' && r.IsGamepadButtonPressed(0, mappedInput[button].button)) {
+        currentScene.buttonPress(button)
+      }
+      if (typeof mappedInput[button].key !== 'undefined' && r.IsKeyPressed(mappedInput[button].key)) {
+        currentScene.buttonPress(button)
+      }
+      if (typeof mappedInput[button].axis !== 'undefined') {
+        if (mappedInput[button].direction === -1 && r.GetGamepadAxisMovement(0, mappedInput[button].axis) < 0 && !pushOnce[mappedInput[button].axis]) {
+          pushOnce[mappedInput[button].axis] = true
+          currentScene.buttonPress(button)
+        }
+        if (mappedInput[button].direction === 1 && r.GetGamepadAxisMovement(0, mappedInput[button].axis) > 0 && !pushOnce[mappedInput[button].axis]) {
+          pushOnce[mappedInput[button].axis] = true
+          currentScene.buttonPress(button)
+        }
+      }
+    }
+    
+    if (currentScene.buttonRelease) {
+      if (typeof mappedInput[button].button !== 'undefined' && r.IsGamepadButtonReleased(0, mappedInput[button].button)) {
+        currentScene.buttonRelease(button)
+      }
+      if (typeof mappedInput[button].key !== 'undefined' && r.IsKeyReleased(mappedInput[button].key)) {
+        currentScene.buttonRelease(button)
+      }
+      if (typeof mappedInput[button].axis !== 'undefined') {
+        if (mappedInput[button].direction === -1 && r.GetGamepadAxisMovement(0, mappedInput[button].axis) === 0 && pushOnce[mappedInput[button].axis]) {
+          pushOnce[mappedInput[button].axis] = false
+          currentScene.buttonRelease(button)
+        }
+        if (mappedInput[button].direction === 1 && r.GetGamepadAxisMovement(0, mappedInput[button].axis) === 0 && pushOnce[mappedInput[button].axis]) {
+          pushOnce[mappedInput[button].axis] = false
+          currentScene.buttonRelease(button)
+        }
+      }
+    }
+  }
+
+  if (currentScene.update) {
+    currentScene.update()
+  }
+
+  r.BeginDrawing()
+  
+  if (currentScene.draw) {
+    currentScene.draw()
+  }
+
+  r.EndDrawing()
 }
 
-const axis = [0, 0]
+if (currentScene.destroy) {
+  currentScene.destroy()
+}
 
-// I do a weird interval to make the game-loop work better with async stuff
-const interval = setInterval(() => {
-  if (r.WindowShouldClose() || globalThis.shouldClose) {
-    return exit()
-  } else {
-    // on keyboard exit on START + SELECT
-    if (r.IsKeyPressed(mappedInput.START.key) && r.IsKeyPressed(mappedInput.SELECT.key)) {
-      return exit()
-    }
-    if (r.IsGamepadAvailable(0)) {
-      // on joystick exit on START + SELECT
-      if (r.IsGamepadButtonDown(0, mappedInput.START.gamepad) && r.IsGamepadButtonDown(0, mappedInput.SELECT.gamepad)) {
-        return exit()
-      }
-      for (const b of Object.keys(mappedInput)) {
-        if (typeof mappedInput[b].gamepad !== 'undefined') {
-          if (currentScene.buttonPress && r.IsGamepadButtonPressed(0, mappedInput[b].gamepad)) {
-            currentScene.buttonPress(b)
-          }
-          if (currentScene.buttonRelease && r.IsGamepadButtonReleased(0, mappedInput[b].gamepad)) {
-            currentScene.buttonRelease(b)
-          }
-        }
-      }
-
-      const ca = [
-        r.GetGamepadAxisMovement(0, 0),
-        r.GetGamepadAxisMovement(0, 1)
-      ]
-
-      if (currentScene.buttonPress) {
-        if (ca[0] < 0) {
-          if (axis[0] === 0) {
-            axis[0] = -1
-            currentScene.buttonPress('UP')
-          }
-        }
-        if (ca[0] > 0) {
-          if (axis[0] === 0) {
-            axis[0] = 1
-            currentScene.buttonPress('DOWN')
-          }
-        }
-        if (ca[1] < 0) {
-          if (axis[1] === 0) {
-            axis[1] = -1
-            currentScene.buttonPress('LEFT')
-          }
-        }
-        if (ca[1] > 0) {
-          if (axis[1] === 0) {
-            axis[1] = 1
-            currentScene.buttonPress('RIGHT')
-          }
-        }
-      }
-      if (currentScene.buttonRelease) {
-        if (ca[0] === 0) {
-          if (axis[0] === -1) {
-            axis[0] = 0
-            currentScene.buttonRelease('UP')
-          }
-          if (axis[0] === 1) {
-            axis[0] = 0
-            currentScene.buttonRelease('DOWN')
-          }
-        }
-        if (ca[1] === 0) {
-          if (axis[0] === -1) {
-            axis[1] = 0
-            currentScene.buttonRelease('LEFT')
-          }
-          if (axis[1] === 1) {
-            axis[1] = 0
-            currentScene.buttonRelease('RIGHT')
-          }
-        }
-      }
-    }
-
-    for (const b of Object.keys(mappedInput)) {
-      if (typeof mappedInput[b].gamepad !== 'undefined') {
-        if (currentScene.buttonPress && r.IsKeyPressed(mappedInput[b].key)) {
-          currentScene.buttonPress(b)
-        }
-        if (currentScene.buttonRelease && r.IsKeyReleased(mappedInput[b].key)) {
-          currentScene.buttonRelease(b)
-        }
-      }
-    }
-
-    if (currentScene.update) {
-      currentScene.update()
-    }
-
-    r.BeginDrawing()
-    if (currentScene.draw) {
-      currentScene.draw()
-    }
-
-    r.DrawText(r.GetGamepadButtonPressed().toString(), 310, 10, 8, r.WHITE)
-
-    r.EndDrawing()
-  }
-}, 10)
+r.CloseWindow()
